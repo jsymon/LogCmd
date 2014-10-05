4f4670693145e0ea196ac9b137981d0175a10a2d
jsymon <james_symon@hotmail.com>
2014-10-05 16:10:04 +0100

    Email component

--- LogCmd/Program.cs
+++ LogCmd/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Diagnostics;
-using System.IO;
 namespace LogCmd
 {
     class Program
     {
+        /// <summary>
+        /// Console standard output
+        /// </summary>
         private static string _output = null;
+
+        /// <summary>
+        /// Console error output
+        /// </summary>
+        private static string _error = null;
 
         private static void Main(string[] args)
         {
@@ -26,24 +30,24 @@ namespace LogCmd
             {
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
+                RedirectStandardError = true,
                 CreateNoWindow = true,
                 WindowStyle = ProcessWindowStyle.Hidden,
                 FileName = "cmd.exe",
                 Arguments = "/C " + string.Join(" ", args)
             };
 
+            processArguments.Arguments = "/C test.bat";
+
             process.StartInfo = processArguments;
             process.Start();
             _output = process.StandardOutput.ReadToEnd();
+            _error = process.StandardError.ReadToEnd();
             process.WaitForExit();
-            var code = process.ExitCode;
             //TODO: write file output (EXIT /B %ERRORLEVEL%)
         }
 
-        private static void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
-        {
-
-        }
+        private static void process_ErrorDataReceived(object sender, DataReceivedEventArgs e) { }
 
         private static void process_Exited(object sender, EventArgs e)
         {
@@ -51,8 +55,36 @@ namespace LogCmd
             if (sender != null
                 && (process = sender as Process) != null)
             {
-                //TODO: Perform action if errorcode > 0, or look elsewhere?
+                OnExit(process);
             }
+        }
+
+        private static void OnExit(Process process)
+        {
+            //JS:If exitcode is equal to 0 then assume everything completed OK
+            if (process.ExitCode == 0)
+                return;
+            //JS:Else log error (usually exitCode = 0)
+            EmailError(process);
+        }
+
+        private static void EmailError(Process process)
+        {
+            string subject = string.Format("An error occurred whilst running {0} via {1}.", process.StartInfo.Arguments, process.StartInfo.FileName);
+            string body =
+                "Standard output:"
+                + Environment.NewLine
+                + "{0}"
+                + Environment.NewLine + Environment.NewLine
+                + "Error output:"
+                + Environment.NewLine
+                + "{1}"
+                + _error;
+
+            var sm = new SendMail(ConfigurationManager.AppSettings["SendMail.From"], ConfigurationManager.AppSettings["SendMail.To"]);
+            sm.Subject = subject;
+            sm.Body = body;
+            sm.Send();
         }
     }
 }
